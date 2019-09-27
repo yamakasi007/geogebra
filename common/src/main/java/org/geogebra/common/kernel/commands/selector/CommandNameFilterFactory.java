74c8c0f2c43dba8c01467c154bbd2ecd6ab30695
Sütő Ágoston <agoston@geogebra.at>
2019-09-27 11:47:00 +0200

    Revert "Merge branch 'apps-1203' into 'master'"
    This reverts merge request !106

--- common/src/main/java/org/geogebra/common/kernel/commands/selector/CommandNameFilterFactory.java
+++ common/src/main/java/org/geogebra/common/kernel/commands/selector/CommandNameFilterFactory.java
@@ -50,10 +50,11 @@ public final class CommandNameFilterFactory {
 		CommandNameFilterSet commandNameFilter = new CommandNameFilterSet(true);
 		commandNameFilter.addCommands(
 				// CAS specific command
-				Commands.Delete, Commands.Max,
-				Commands.Min, Commands.PerpendicularVector,
-				Commands.OrthogonalVector, Commands.Poisson,
-				Commands.UnitPerpendicularVector, Commands.UnitOrthogonalVector,
+				Commands.Delete, Commands.Invert, Commands.Max,
+				Commands.Min, Commands.NSolutions, Commands.PerpendicularVector,
+				Commands.OrthogonalVector, Commands.Poisson, Commands.ReducedRowEchelonForm,
+				Commands.Sequence, Commands.Substitute, Commands.Sum,
+				Commands.Transpose, Commands.UnitPerpendicularVector, Commands.UnitOrthogonalVector,
 				Commands.UnitVector,
 				// Function Commands
 				Commands.Asymptote, Commands.CurvatureVector, Commands.DataFunction,
@@ -73,7 +74,8 @@ public final class CommandNameFilterFactory {
 				Commands.Point, Commands.Polygon, Commands.Polyline,
 				Commands.PolyLine, Commands.ProveDetails, Commands.Ray,
 				Commands.RigidPolygon, Commands.Sector, Commands.Segment,
-				Commands.Slope, Commands.TriangleCurve, Commands.Vertex,
+				Commands.Slope, Commands.Tangent, Commands.TriangleCurve,
+				Commands.Vertex,
 				// Transformation Commands
 				Commands.Rotate, Commands.Shear, Commands.Translate,
 				// Statistics Commands
@@ -96,7 +98,7 @@ public final class CommandNameFilterFactory {
 				Commands.DynamicCoordinates, Commands.Object, Commands.SlowPlot,
 				Commands.ToolImage,
 				// List Commands
-				Commands.IndexOf, Commands.Take,
+				Commands.IndexOf, Commands.RemoveUndefined, Commands.Take,
 				Commands.Zip,
 				// Text Commands
 				Commands.ContinuedFraction, Commands.FormulaText, Commands.LaTeX,
