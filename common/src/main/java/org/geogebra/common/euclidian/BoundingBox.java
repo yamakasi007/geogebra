6aa4b5370e4f78e9a9a9ce3a6f06cd4f91c0f03c
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2019-10-10 09:50:54 +0200

    add back default color

--- common/src/main/java/org/geogebra/common/euclidian/BoundingBox.java
+++ common/src/main/java/org/geogebra/common/euclidian/BoundingBox.java
@@ -17,6 +17,7 @@ import org.geogebra.common.euclidian.draw.DrawSegment;
 import org.geogebra.common.euclidian.draw.DrawWidget;
 import org.geogebra.common.factories.AwtFactory;
 import org.geogebra.common.kernel.geos.GeoElement;
+import org.geogebra.common.main.GeoGebraColorConstants;
 
 /**
  * bounding box construction for selected elements
@@ -59,6 +60,7 @@ public class BoundingBox {
 			this.isImage = isImage;
 			setCropHandlers(new ArrayList<GGeneralPath>());
 		}
+		setColor(GeoGebraColorConstants.GEOGEBRA_ACCENT);
 	}
 
 	/**
