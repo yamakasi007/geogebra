328e3ca7b2ac29fe3c587f6158e0055762f467d0
László Gál <laszlo@geogebra.at>
2019-10-11 10:35:58 +0200

    Apply suggestion to common/src/main/java/org/geogebra/common/kernel/algos/AlgoCasLoaded.java

--- common/src/main/java/org/geogebra/common/kernel/algos/AlgoCasLoaded.java
+++ common/src/main/java/org/geogebra/common/kernel/algos/AlgoCasLoaded.java
@@ -19,6 +19,8 @@ public class AlgoCasLoaded extends AlgoElement implements UsesCAS {
 		output = new GeoBoolean(cons, false);
 		setOutputLength(1);
 		setOutput(0, output);
+		input = new GeoElement[0];
+		setDependencies();
 	}
 
 	@Override
