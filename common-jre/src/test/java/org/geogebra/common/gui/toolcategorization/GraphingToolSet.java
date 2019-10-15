dafd2576c58ba79a96512ad59fb7829830486682
janborovsky <janborovsky01@gmail.com>
2019-10-15 12:31:06 +0200

    APPS-1343 GraphingToolSet comment updated

--- common-jre/src/test/java/org/geogebra/common/gui/toolcategorization/GraphingToolSet.java
+++ common-jre/src/test/java/org/geogebra/common/gui/toolcategorization/GraphingToolSet.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Base class for Graphing ToolSet tests.
+ * Utility class for Graphing ToolSet tests.
  */
 public class GraphingToolSet {
 
