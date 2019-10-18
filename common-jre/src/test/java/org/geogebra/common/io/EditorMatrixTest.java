235a4cebadeb1baf5f662253768a1bf9a2499b3a
Laszlo Gal <laszlo@geogebra.org>
2019-10-18 14:11:57 +0200

    EditorMatrixTest

---
+++ common-jre/src/test/java/org/geogebra/common/io/EditorMatrixTest.java
@@ -0,0 +1,16 @@
+package org.geogebra.common.io;
+
+import org.junit.Test;
+
+public class EditorMatrixTest extends BaseEditorTest {
+	private static final String matix3x3 = "{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}";
+
+	@Test
+	public void testMatrix() {
+		type(matix3x3)
+				.checkRaw("MathSequence[MathArray[MathSequence[" +
+						"MathArray[MathSequence[1, ,,  , 2, ,,  , 3]], ,,  , " +
+						"MathArray[MathSequence[4, ,,  , 5, ,,  , 6]], ,,  , " +
+						"MathArray[MathSequence[7, ,,  , 8, ,,  , 9]]]]]");
+	}
+}
