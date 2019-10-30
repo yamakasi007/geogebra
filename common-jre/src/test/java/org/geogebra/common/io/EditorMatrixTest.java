49690427d02160918452a462f0b6ce531f89cdb3
Laszlo Gal <laszlo@geogebra.org>
2019-10-30 15:08:34 +0100

    APPS-1316 Eliminating inheritance from the tests

--- common-jre/src/test/java/org/geogebra/common/io/EditorMatrixTest.java
+++ common-jre/src/test/java/org/geogebra/common/io/EditorMatrixTest.java
@@ -1,54 +1,65 @@
 package org.geogebra.common.io;
 
+import org.geogebra.common.AppCommonFactory;
+import org.geogebra.common.jre.headless.AppCommon;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.himamis.retex.editor.share.util.JavaKeyCodes;
 
-public class EditorMatrixTest extends BaseEditorTest {
+public class EditorMatrixTest {
 	private static final String matix3x3 = "{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}";
+	private static EditorChecker checker;
+	private static AppCommon app = AppCommonFactory.create();
+
+	@Before
+	public void setUp() {
+		checker = new EditorChecker(app);
+	}
 
 	@Test
 	public void testCaretInitialPosition() {
-		fromParser(matix3x3).checkPath(0,0,0);
+		checker.fromParser(matix3x3).checkPath(0,0,0);
 	}
 
 	@Test
 	public void testCaretShouldStayInsideOnHomeInitially() {
-		fromParser(matix3x3).typeKey(JavaKeyCodes.VK_HOME).checkPath(0,0,0);
+		checker.fromParser(matix3x3).typeKey(JavaKeyCodes.VK_HOME).checkPath(0,0,0);
 	}
 
 	@Test
 	public void testCaretShouldStayInsideOnHome() {
-		fromParser(matix3x3)
+		checker.fromParser(matix3x3)
 				.repeatKey(JavaKeyCodes.VK_RIGHT, 6)
 				.typeKey(JavaKeyCodes.VK_HOME).checkPath(0, 0, 0);
 	}
-		@Test
+
+	@Test
 	public void testCaretShouldStayInsideOnEnd() {
-		fromParser(matix3x3).typeKey(JavaKeyCodes.VK_END).checkPath(1, 8, 0);
+		checker.fromParser(matix3x3).typeKey(JavaKeyCodes.VK_END).checkPath(1, 8, 0);
 	}
 
 	@Test
 	public void testCaretShouldStayInsideOnLeftArrow() {
-		fromParser(matix3x3)
+		checker.fromParser(matix3x3)
 			.repeatKey(JavaKeyCodes.VK_RIGHT, 6)
 			.repeatKey(JavaKeyCodes.VK_LEFT, 20).checkPath(0, 0, 0);
 	}
 
 	@Test
 	public void testCaretShouldStayInsideOnRightArrow() {
-		fromParser(matix3x3).repeatKey(JavaKeyCodes.VK_RIGHT, 20).checkPath(1, 8, 0);
+		checker.fromParser(matix3x3).repeatKey(JavaKeyCodes.VK_RIGHT, 20).checkPath(1, 8, 0);
 	}
 
 	@Test
 	public void testCaretShouldStayInsideOnUpArrow() {
-		fromParser(matix3x3)
+		checker.fromParser(matix3x3)
 				.repeatKey(JavaKeyCodes.VK_DOWN, 2)
 				.repeatKey(JavaKeyCodes.VK_UP, 20).checkPath(0, 0, 0);
 	}
 
 	@Test
 	public void testCaretShouldStayInsideOnDownArrow() {
-		fromParser(matix3x3).repeatKey(JavaKeyCodes.VK_DOWN, 20).checkPath(0, 6, 0);
+		checker.fromParser(matix3x3).repeatKey(JavaKeyCodes.VK_DOWN, 20).checkPath(0, 6, 0);
 	}
 }
