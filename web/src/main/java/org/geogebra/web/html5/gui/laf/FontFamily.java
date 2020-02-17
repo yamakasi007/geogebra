bec725d2f1f4e6748c5534f407a3c8426f8e7b2f
Laszlo Gal <laszlo@geogebra.org>
2020-02-17 14:04:31 +0100

    font fallbacks

--- web/src/main/java/org/geogebra/web/html5/gui/laf/FontFamily.java
+++ web/src/main/java/org/geogebra/web/html5/gui/laf/FontFamily.java
@@ -1,23 +1,23 @@
 package org.geogebra.web.html5.gui.laf;
 
 public enum FontFamily {
-	ARIAL("Arial", "arial"),
-	CALIBRI("Calibri", "Calibri"),
-	CENTURY_GOTHIC("Century Gothic", "Century Gothic"),
-	COMIC_SANS("Comic Sans", "Comic Sans MS"),
-	COURIER("Courier", "Courier"),
-	GEORGIA("Georgia", "Georgia"),
+	ARIAL("Arial", "arial, sans-serif"),
+	CALIBRI("Calibri", "Calibri, sans-serif"),
+	CENTURY_GOTHIC("Century Gothic", "Century Gothic, sans-serif"),
+	COMIC_SANS("Comic Sans", "Comic Sans MS, sans-serif"),
+	COURIER("Courier", "Courier, monospace"),
+	GEORGIA("Georgia", "Georgia, serif"),
 	DYSLEXIC("Open dyslexic mit Fibel a", "Open dyslexic mit Fibel a"),
-	PALATINO("Palatino", "Palatino Linotype"),
-	QUICKSAND("Qicksand", "Qicksand"),
-	ROBOTO(	"Roboto", "Roboto"),
-	SCHULBUCH_BAYERN("Schulbuch Bayern", "Schulbuch Bayern"),
-	SF_MONO("SF Mono", "SF Mono"),
-	SF_PRO("SF Pro", "SF Pro"),
-	TIMES("Times", "Times"),
-	TITILIUM("Titilium Web", "Titilium Web"),
-	TREBUCHET("Trebuchet", "Trebuchet MS"),
-	VERDANA("Verdana", "Verdana");
+	PALATINO("Palatino", "Palatino Linotype, serif"),
+	QUICKSAND("Qicksand", "Qicksand, sans-serif"),
+	ROBOTO(	"Roboto", "Roboto, sans-serif"),
+	SCHULBUCH_BAYERN("Schulbuch Bayern", "Schulbuch Bayern, sans-serif"),
+	SF_MONO("SF Mono", "SF Mono, monospace"),
+	SF_PRO("SF Pro", "SF Pro, sans-serif"),
+	TIMES("Times", "Times, serif"),
+	TITILIUM("Titilium Web", "Titilium Web, sans-serif"),
+	TREBUCHET("Trebuchet", "Trebuchet MS, sans-serif"),
+	VERDANA("Verdana", "Verdana, sans-serif");
 	private String displayName;
 	private String cssName;
 
