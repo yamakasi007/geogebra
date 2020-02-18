d1f7ce776c0ab251136a62d6a3397e7bcccb4435
Csilla Solyom Gecse <csilla.solyom-gecse@geogebra.at>
2020-02-18 13:02:50 +0100

    Model class for groups (MOW-959)

--- common/src/main/java/org/geogebra/common/io/MyXMLHandler.java
+++ common/src/main/java/org/geogebra/common/io/MyXMLHandler.java
@@ -2916,6 +2916,8 @@ public class MyXMLHandler implements DocHandler {
 			} else if ("cascell".equals(eName)) {
 				constMode = MODE_CONST_CAS_CELL;
 				casMode = MODE_CONST_CAS_CELL;
+			} else if ("group".equals(eName)) {
+				geoHandler.handleGroup(attrs);
 			} else if ("worksheetText".equals(eName)) {
 				handleWorksheetText(attrs);
 			} else {
