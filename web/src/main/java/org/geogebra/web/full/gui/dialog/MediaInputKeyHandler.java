843e6b0cf4d5627bb32d8b7fb15cde440aa6f3a4
Laszlo Gal <laszlo@geogebra.org>
2020-02-20 11:30:53 +0100

    KeyHandler

---
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/MediaInputKeyHandler.java
@@ -0,0 +1,60 @@
+package org.geogebra.web.full.gui.dialog;
+
+import org.geogebra.web.html5.gui.inputfield.AutoCompleteTextFieldW;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.KeyCodeEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+
+/**
+ * Input handlers of a textfield.
+ * @author laszlo
+ */
+public class MediaInputKeyHandler implements KeyUpHandler, KeyDownHandler {
+
+	private final ProcessInput input;
+
+	/**
+	 *
+	 * @param field to check
+	 * @param processInput the process, input change code.
+	 */
+	public MediaInputKeyHandler(AutoCompleteTextFieldW field,
+								ProcessInput processInput) {
+		this.input = processInput;
+
+		field.getTextBox().addKeyUpHandler(this);
+		field.getTextBox().addKeyDownHandler(this);
+		addNativeInputHandler(field.getInputElement());
+	}
+
+	private native void addNativeInputHandler(Element elem) /*-{
+		var that = this;
+		elem.addEventListener("input", function () {
+			that.@org.geogebra.web.full.gui.dialog.MediaInputPanel::onInput()();
+		});
+	}-*/;
+
+	@Override
+	public void onKeyDown(KeyDownEvent event) {
+		if (isEnterKey(event)) {
+			input.processInput();
+		}
+	}
+
+	private boolean isEnterKey(KeyCodeEvent<?> event) {
+		return event.getNativeEvent()
+				.getKeyCode() == KeyCodes.KEY_ENTER;
+	}
+
+	@Override
+	public void onKeyUp(KeyUpEvent event) {
+		if (!isEnterKey(event)) {
+			input.onInput();
+		}
+	}
+}
