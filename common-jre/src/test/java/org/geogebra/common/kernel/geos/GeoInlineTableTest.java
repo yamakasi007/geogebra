7b6e9e413fc60c8d65ad4c02e90b545e3cec4476
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-05-07 12:38:45 +0200

    remove not neded part

--- common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoInlineTableTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoInlineTableTest.java
@@ -11,24 +11,6 @@ import org.junit.Test;
 
 public class GeoInlineTableTest extends BaseUnitTest {
 
-	private static final String COMPATIBILITY_XML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
-			+ "<geogebra format=\"5.0\" version=\"5.0.580.0\" app=\"notes\" >\n"
-			+ "<gui>\n"
-			+ "\t<font  size=\"16\"/>\n"
-			+ "</gui>\n"
-			+ "<construction>\n"
-			+ "<expression label=\"text1\" exp=\"&quot;GeoGebra Rocks&quot;\"/>\n"
-			+ "<element type=\"table\" label=\"testTable\">\n"
-			+ "\t<show object=\"true\" label=\"true\">\n"
-			+ "\t<objColor r=\"0\" g=\"0\" b=\"0\" alpha=\"0\"/>\n"
-			+ "\t<layer val=\"0\"/>\n"
-			+ "\t<labelMode val=\"4\"/>\n"
-			+ "\t<startPoint  x=\"5\" y=\"6\" z=\"1\"/>\n"
-			+ "\t<dimensions width=\"200.0\" height=\"72.0\" angle=\"0.0\"/>\n"
-			+ "</element>\n"
-			+ "</construction>\n"
-			+ "</geogebra>";
-
 	@Test
 	public void inlineTableCorrectlySavedAndLoaded() {
 		final double x = 1.2;
@@ -59,18 +41,4 @@ public class GeoInlineTableTest extends BaseUnitTest {
 		assertEquals(height, loadedInlineTable.getHeight(), Kernel.MAX_PRECISION);
 		assertEquals(angle, loadedInlineTable.getAngle(), Kernel.MAX_PRECISION);
 	}
-
-	/*@Test
-	public void loadingOldXmlShouldProduceInlineTexts() {
-		getApp().setXML(COMPATIBILITY_XML, true);
-
-		GeoInlineText loadedInlineText = (GeoInlineText) lookup("text1");
-
-		assertEquals(5, loadedInlineText.getLocation().getX(), Kernel.MAX_PRECISION);
-		assertEquals(6, loadedInlineText.getLocation().getY(), Kernel.MAX_PRECISION);
-		assertEquals(150, loadedInlineText.getWidth(), Kernel.MAX_PRECISION);
-		assertEquals(50, loadedInlineText.getHeight(), Kernel.MAX_PRECISION);
-		assertEquals("[{\"text\":\"GeoGebra Rocks\",\"bold\":true}]",
-				loadedInlineText.getContent());
-	}*/
 }
