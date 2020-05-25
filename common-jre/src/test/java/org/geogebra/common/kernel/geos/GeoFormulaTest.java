b6e3fb4f1cedc8178c1e34192cade955512ec52e
csillaSolyomGecse <39436734+csillaSolyomGecse@users.noreply.github.com>
2020-05-25 09:23:35 +0200

    MOW-1003 test vector

--- common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoFormulaTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/geos/GeoFormulaTest.java
@@ -7,9 +7,20 @@ import org.geogebra.common.awt.GPoint2D;
 import org.geogebra.common.io.XmlTestUtil;
 import org.geogebra.common.kernel.Construction;
 import org.geogebra.common.kernel.Kernel;
+import org.geogebra.common.kernel.StringTemplate;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GeoFormulaTest extends BaseUnitTest {
+
+	private Construction cons;
+	private GeoFormula equationEditor;
+
+	@Before
+	public void setupTest() {
+		cons = getApp().getKernel().getConstruction();
+		equationEditor = new GeoFormula(cons, null);
+	}
 
 	@Test
 	public void formulaCorrectlySavedAndLoaded() {
@@ -19,8 +30,6 @@ public class GeoFormulaTest extends BaseUnitTest {
 		final int height = 1956;
 		final double angle = 2.7182;
 		final String content = "e^(i*pi)+1=1";
-
-		Construction cons = getApp().getKernel().getConstruction();
 
 		GPoint2D startPoint = new GPoint2D(x, y);
 
@@ -43,5 +52,12 @@ public class GeoFormulaTest extends BaseUnitTest {
 		assertEquals(height, loadedFormula.getHeight(), Kernel.MAX_PRECISION);
 		assertEquals(angle, loadedFormula.getAngle(), Kernel.MAX_PRECISION);
 		assertEquals(content, loadedFormula.getContent());
+	}
+
+	@Test
+	public void vector() {
+		equationEditor.setContent("vec(AB)=b-a");
+		assertEquals("\\overrightarrow{AB}\\,=\\,b-a",
+				equationEditor.toValueString(StringTemplate.latexTemplate));
 	}
 }
