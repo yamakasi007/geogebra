8606337422a27cc916a99bc663fe1a867ff3e8db
Laszlo Gal <latz.gal@gmail.com>
2020-10-09 16:01:48 +0200

    APPS-2377 multiply, division, tests

--- common-jre/src/test/java/org/geogebra/common/kernel/interval/IntervalTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/interval/IntervalTest.java
@@ -8,19 +8,81 @@ public class IntervalTest {
 
 	@Test
 	public void testAdd() {
-		Interval interval = new Interval(-3, 2);
-		Interval argument = new Interval(4, 7);
-		Interval result = new Interval(1, 9);
-		interval.add(argument);
-		assertEquals(result, interval);
+		assertEquals(interval(1, 9),
+				interval(-3, 2)
+						.add(interval(4, 7)));
+	}
+
+	private Interval interval(double low, double high) {
+		return new Interval(low, high);
 	}
 
 	@Test
 	public void testSub() {
-		Interval interval = new Interval(-1, 3);
-		Interval argument = new Interval(-2, 4);
-		Interval result = new Interval(-5, 5);
-		interval.sub(argument);
-		assertEquals(result, interval);
+		assertEquals(interval(-5, 5),
+				interval(-1, 3)
+						.sub(interval(-2, 4)));
+	}
+
+	@Test
+	public void testMultiplication1() {
+		assertEquals(interval(-5, 10),
+				interval(-1, 2)
+						.multiply(interval(3, 5)));
+	}
+
+	@Test
+	public void testMultiplication2() {
+		assertEquals(interval(-8, -3),
+				interval(-4, -3)
+						.multiply(interval(1, 2)));
+
+	}
+
+	@Test
+	public void testMultiplication3() {
+		assertEquals(interval(-15, 10),
+				interval(-5, 1)
+						.multiply(interval(-2, 3)));
+
+	}
+
+	@Test
+	public void testMultiplication4() {
+		assertEquals(interval(-12, 6),
+				interval(-1, 2)
+						.multiply(interval(-6, -4)));
+
+	}
+
+	@Test
+	public void testMultiplication5() {
+		assertEquals(interval(0, 7),
+				interval(-7, -5)
+						.multiply(interval(-1, 0)));
+
+	}
+
+	@Test
+	public void testMultiplyThreeIntervals() {
+		assertEquals(interval(-30, 60),
+				interval(-3, 1)
+						.multiply(interval(-2, 4))
+						.multiply(interval(-5, -1)));
+
+	}
+
+	@Test(expected = IntervalDivisionByZero.class)
+	public void testDivisionByZero() throws IntervalDivisionByZero {
+		interval(-1/3.0, 1/4.0)
+				.div(interval(-1.0, 1.0));
+	}
+
+	@Test
+	public void testDivision() throws IntervalDivisionByZero {
+		assertEquals(interval(-2/3.0, 1/2.0),
+				interval(-1/3.0, 1/4.0)
+						.div(interval(1/2.0, 3/4.0)));
+
 	}
 }
--- common/src/main/java/org/geogebra/common/kernel/interval/Interval.java
+++ common/src/main/java/org/geogebra/common/kernel/interval/Interval.java
@@ -1,5 +1,8 @@
 package org.geogebra.common.kernel.interval;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class Interval {
@@ -16,9 +19,10 @@ public class Interval {
 	 *
 	 * @param other interval to add
 	 */
-	public void add(Interval other) {
+	public Interval add(Interval other) {
 		low += other.low;
 		high += other.high;
+		return this;
 	}
 
 	@Override
@@ -35,8 +39,45 @@ public class Interval {
 		return Objects.hash(low, high);
 	}
 
-	public void sub(Interval argument) {
-		low -= argument.high;
-		high -= argument.low;
+	@Override
+	public String toString() {
+		return "Interval [" +
+				low +
+				", "
+				+ high +
+				']';
+	}
+
+	public Interval sub(Interval other) {
+		low -= other.high;
+		high -= other.low;
+		return this;
+	}
+
+	public Interval multiply(Interval other) {
+		getBoundsFromList(Arrays.asList(low * other.low, low * other.high, high * other.low,
+				high * other.high));
+		return this;
+	}
+
+	protected void getBoundsFromList(List<Double> list) {
+		Collections.sort(list);
+
+		// to avoid -0.0, see testMultiplication5
+		low = list.get(0) + 0.0;
+		high = list.get(3) + 0.0;
+	}
+
+	public Interval div(Interval other) throws IntervalDivisionByZero {
+		if (other.hasZero()) {
+			throw new IntervalDivisionByZero();
+		}
+		getBoundsFromList(Arrays.asList(low / other.low, low / other.high, high / other.low,
+				high / other.high));
+		return this;
+	}
+
+	private boolean hasZero() {
+		return low <= 0 && high >= 0;
 	}
 }
