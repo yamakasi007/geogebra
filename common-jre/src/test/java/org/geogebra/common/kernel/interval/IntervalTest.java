ac929d59362ac33a0163cd04b9325f6c7d007868
Laszlo Gal <latz.gal@gmail.com>
2020-10-12 15:17:50 +0200

    APPS-2377 test checkStyle fixes.

--- common-jre/src/test/java/org/geogebra/common/kernel/interval/IntervalTest.java
+++ common-jre/src/test/java/org/geogebra/common/kernel/interval/IntervalTest.java
@@ -90,15 +90,15 @@ public class IntervalTest {
 
 	@Test(expected = IntervalDivisionByZero.class)
 	public void testDivisionByZero() throws IntervalDivisionByZero {
-		interval(-1/3.0, 1/4.0)
+		interval(-1 / 3.0, 1 / 4.0)
 				.divide(interval(-1.0, 1.0));
 	}
 
 	@Test
 	public void testDivision() throws IntervalDivisionByZero {
-		assertEquals(interval(-2/3.0, 1/2.0),
-				interval(-1/3.0, 1/4.0)
-						.divide(interval(1/2.0, 3/4.0)));
+		assertEquals(interval(-2 / 3.0, 1 / 2.0),
+				interval(-1 / 3.0, 1 / 4.0)
+						.divide(interval(1 / 2.0, 3 / 4.0)));
 
 	}
 
