c7d677ce71e3b29e2d5dab7018faf6784e64cc7c
laszlo <laszlo@geogebra.at>
2020-11-17 15:55:25 +0100

    APPS-2469 adding IntervalPathPlotterMock

---
+++ common-jre/src/test/java/org/geogebra/common/euclidian/plot/interval/IntervalPathPlotterMock.java
@@ -0,0 +1,38 @@
+package org.geogebra.common.euclidian.plot.interval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.geogebra.common.awt.GGraphics2D;
+import org.geogebra.common.util.StringUtil;
+
+public class IntervalPathPlotterMock implements IntervalPathPlotter {
+	private List<String> log = new ArrayList<>();
+
+	@Override
+	public void reset() {
+		log.clear();
+		log.add("R");
+	}
+
+	@Override
+	public void moveTo(double x, double y) {
+		log.add("M " + x + " " + y);
+	}
+
+	@Override
+	public void lineTo(double x, double y) {
+		log.add("L " + x + " " + y);
+
+	}
+
+	@Override
+	public void draw(GGraphics2D g2) {
+		// stub.
+	}
+
+	public String getLog() {
+		return StringUtil.join(",", log);
+	}
+}
+
